6c42b5db07dc30d4ca0dd312f3fce4f09cb959dd
phuc94 <trungphuc94@gmail.com>
2022-12-13 22:18:43 +0700

    save

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ const queriesPath = "./redux/actions/queries/"
 const actionsPath = "./redux/actions/"
 
 
-// const queries_files = cli(` find ${queriesPath} -maxdepth 2 -name "*.js" | sed '/index/d' `)
-// queries_files.forEach(filePath => {
-//   cli(`jscodeshift -t codemod/transform-queries ${filePath} `)
-// })
-//
-//
-// const actions_files = cli(` find ${actionsPath} -maxdepth 1 -name "*.js" | sed '/index/d' `)
+const queries_files = cli(` find ${queriesPath} -maxdepth 2 -name "*.js" | sed '/index/d' `)
+queries_files.forEach(filePath => {
+  cli(`jscodeshift -t codemod/transform-queries ${filePath} `)
+})
+
+
+const actions_files = cli(` find ${actionsPath} -maxdepth 1 -name "*.js" | sed '/index/d' `)
 const queries_loc = cli(`codemod/transform-actions/import-prep.sh ${queriesPath}`)
-// actions_files.forEach(filePath => {
-//   const usingQueriesRaw = cli(`jscodeshift -t codemod/transform-actions ${filePath} `)
-//   const usingQueries = getQueries(usingQueriesRaw, "QUERY_MARK", "|")
-//   if (usingQueries) {
-//     cli(`codemod/transform-actions/import.sh ${filePath} ${usingQueries} ${queries_loc}`)
-//   }
-// })
-//
-// cli(`codemod/transform-actions/import-capitals.sh`)
+actions_files.forEach(filePath => {
+  const usingQueriesRaw = cli(`jscodeshift -t codemod/transform-actions ${filePath} `)
+  const usingQueries = getQueries(usingQueriesRaw, "QUERY_MARK", "|")
+  if (usingQueries) {
+    cli(`codemod/transform-actions/import.sh ${filePath} ${usingQueries} ${queries_loc}`)
+  }
+})
+
+cli(`codemod/transform-actions/import-capitals.sh`)
 
 console.log(cli(`codemod/transform-actions/import-direct.sh ${queries_loc}`))
 
